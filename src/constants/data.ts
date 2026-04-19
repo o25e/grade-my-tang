0feae9aba2eec8d24c025b683d5ef9b8eefc1107
parkseoyeon9163@gmail.com <162225988+o25e@users.noreply.github.com>
2026-04-19 14:30:24 +0900

    fix: 재료 위치 조정 및 크기 수정

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -2,32 +2,32 @@ import { Ingredient, Sauce, SpiceLevel } from "../types/game";
 
 // ─── 재료 ─────────────────────────────────────────────────────────────────────
 export const MEATS: Ingredient[] = [
-  { id: "beef",    name: "소고기", emoji: "🥩", image: "/img/meet.png",    type: "meat",   position: { top: "32%", left: "28%", width: "90px" } },
-  { id: "spam",    name: "스팸",   emoji: "🥫", image: "/img/spam.png",    type: "meat",   position: { top: "38%", left: "63%", width: "80px" } },
-  { id: "sausage", name: "소세지", emoji: "🌭", image: "/img/sausage.png", type: "meat",   position: { top: "28%", left: "52%", width: "84px" } },
+  { id: "beef",    name: "소고기", emoji: "🥩", image: "/img/meet.png",    type: "meat",   position: { top: "30%", left: "28%", width: "90px" } },
+  { id: "spam",    name: "스팸",   emoji: "🥫", image: "/img/spam.png",    type: "meat",   position: { top: "30%", left: "61%", width: "80px" } },
+  { id: "sausage", name: "소세지", emoji: "🌭", image: "/img/sausage.png", type: "meat",   position: { top: "28%", left: "51%", width: "84px" } },
 ];
 
 export const VEGGIES: Ingredient[] = [
-  { id: "bokchoy", name: "청경채", emoji: "🥬", image: "/img/vege.png",    type: "veggie", position: { top: "35%", left: "18%", width: "76px" } },
-  { id: "sprouts", name: "숙주",   emoji: "🌱", image: "/img/sprouts.png", type: "veggie", position: { top: "42%", left: "38%", width: "80px" } },
-  { id: "cabbage", name: "배추",   emoji: "🥗", image: "/img/cabbage.png", type: "veggie", position: { top: "25%", left: "68%", width: "78px" } },
+  { id: "bokchoy", name: "청경채", emoji: "🥬", image: "/img/vege.png",    type: "veggie", position: { top: "28%", left: "22%", width: "76px" } },
+  { id: "sprouts", name: "숙주",   emoji: "🌱", image: "/img/sprouts.png", type: "veggie", position: { top: "23%", left: "38%", width: "110px" } },
+  { id: "cabbage", name: "배추",   emoji: "🥗", image: "/img/cabbage.png", type: "veggie", position: { top: "27%", left: "65%", width: "73px" } },
 ];
 
 export const NOODLES: Ingredient[] = [
-  { id: "bunmoja",  name: "분모자",   emoji: "🍢", image: "/img/bunmoja.png", type: "noodle", position: { top: "42%", left: "50%", width: "74px" } },
-  { id: "dangmyun", name: "중국당면", emoji: "🍜", image: "/img/china.png",   type: "noodle", position: { top: "40%", left: "25%", width: "76px" } },
-  { id: "corn",     name: "옥수수면", emoji: "🌽", image: "/img/cone.png",    type: "noodle", position: { top: "30%", left: "40%", width: "105px" } },
+  { id: "bunmoja",  name: "분모자",   emoji: "🍢", image: "/img/bunmoja.png", type: "noodle", position: { top: "36%", left: "50%", width: "74px" } },
+  { id: "dangmyun", name: "중국당면", emoji: "🍜", image: "/img/china.png",   type: "noodle", position: { top: "34%", left: "32%", width: "77px" } },
+  { id: "corn",     name: "옥수수면", emoji: "🌽", image: "/img/cone.png",    type: "noodle", position: { top: "28%", left: "40%", width: "105px" } },
 ];
 
 export const OTHERS: Ingredient[] = [
-  { id: "sweetrice",   name: "고구마떡", emoji: "🍡", image: "/img/tteok.png", type: "other", position: { top: "28%", left: "22%", width: "68px" } },
-  { id: "cheesetteok", name: "연근",   emoji: "", image: "/img/lotusroot.png",   type: "other", position: { top: "30%", left: "44%", width: "72px" } },
+  { id: "sweetrice",   name: "고구마떡", emoji: "🍡", image: "/img/tteok.png", type: "other", position: { top: "19%", left: "30%", width: "75px" } },
+  { id: "cheesetteok", name: "연근",   emoji: "", image: "/img/lotusroot.png",   type: "other", position: { top: "30%", left: "44%", width: "80px" } },
   { id: "tofu",        name: "두부",     emoji: "⬜", image: "/img/tofu.png",   type: "other", position: { top: "18%", left: "50%", width: "76px" } },
 ];
 
 export const TRAPS: Ingredient[] = [
-  { id: "mintchoco", name: "초콜릿", emoji: "🍫", image: "/img/chocolate.png",     type: "trap", position: { top: "22%", left: "34%", width: "66px" } },
-  { id: "mushroom",  name: "팽이버섯", emoji: "🍄", image: "/img/mushroom.png", type: "trap", position: { top: "30%", left: "62%", width: "70px" } },
+  { id: "mintchoco", name: "초콜릿", emoji: "🍫", image: "/img/chocolate.png",     type: "trap", position: { top: "20%", left: "36%", width: "66px" } },
+  { id: "mushroom",  name: "팽이버섯", emoji: "🍄", image: "/img/mushroom.png", type: "trap", position: { top: "31%", left: "60%", width: "72px" } },
 ];
 
 export const ALL_ITEMS: Ingredient[] = [
